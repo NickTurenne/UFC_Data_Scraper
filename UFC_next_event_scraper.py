759625d86bae02285bdcf5d542e4558f225df8cd
Nicholas Turenne <nbt1@sfu.ca>
2025-03-03 13:28:20 -0800

    First Commit

---
+++ UFC_next_event_scraper.py
@@ -0,0 +1,49 @@
+import requests as rq
+# import sqlite3 as sql
+from bs4 import BeautifulSoup as bs
+
+# DESC: Retrieve fight info from a matchup from ufcstats.com
+# Preconditions: Valid link to matchup
+# Postconditions: 2D array of fighter stats, element 1 corresponding to the first fighter
+def GetFighterStats(link:str):
+    r = rq.get(link)
+    soup = bs(r.text, 'html.parser')
+    fighterInfo = soup.find('table', {'class' : 'b-fight-details__table'})
+    rows = fighterInfo.find('tr').find_all('a')
+    names = []
+    for row in rows:
+        names.append(row.text.strip())
+
+    statsRows = soup.find_all('tr', {'class' : 'b-fight-details__table-row-preview'})
+    infoFighter1 = []
+    infoFighter1.append(names[0])
+    infoFighter2 = []
+    infoFighter2.append(names[1])
+    for i in range(15):
+        subRows = statsRows[i].find_all('p', {'class' : 'b-fight-details__table-text'})
+        infoFighter1.append(subRows[1].text.strip())
+        infoFighter2.append(subRows[2].text.strip())
+    return [infoFighter1, infoFighter2]
+
+# 1. Get the link to most recent event from ufcstats.com
+r = rq.get('http://ufcstats.com/statistics/events/upcoming')
+soup = bs(r.text, 'html.parser')
+rows = soup.find('table', {'class' : 'b-statistics__table-events'}).find('tbody').find_all('tr')
+top = rows[1]
+link = top.find('td').find('a').get('href')
+
+# 2. Get the fighter matchup links from the main event (will probably want this to be for all fights)
+r = rq.get(link)
+soup = bs(r.text, 'html.parser')
+rows = soup.find('table', {'class' : 'b-fight-details__table b-fight-details__table_style_margin-top b-fight-details__table_type_event-details js-fight-table'}).find('tbody').find_all('td')
+matchup = rows[4]
+link = matchup.find('a').get('data-link')
+
+# 3. Extract the fighter information (this will probably be put into a function)
+
+info = GetFighterStats(link)
+print(info[0])
+print(info[1])
+
+
+
